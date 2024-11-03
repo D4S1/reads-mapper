b845607aaf99e38f1295433b894dfd8a9dbc8745
annnie73 <aniaszymik02@gmail.com>
2024-11-03 17:12:45 +0100

    add genome preprocessing

--- mapper.py
+++ mapper.py
@@ -1,5 +1,15 @@
 from bwt import FmIndex
+from Bio import SeqIO
+import time
 
-# wczytać genom
-# SA
-# BWT (z FM-indeksem)
+start_time = time.time()
+
+genome_path = 'data/reference20M.fasta'
+
+with open(genome_path, 'r') as file:
+    lines = file.readlines()
+genome = "".join(line.strip() for line in lines[1:])
+
+fm_index = FmIndex(genome)
+print(fm_index)
+print(f'Time: {(time.time() - start_time)/60} min')
